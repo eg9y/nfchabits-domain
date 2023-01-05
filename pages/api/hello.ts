195f4cfcf5f3fe5a5feb90d398c2fcdefd95e266
Egan Bisma <brickkace@gmail.com>
2023-01-05 10:38:12 +0700

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "83PBK2ACHB.random1.org.NFCHabits",
+          paths: ["*"],
+        },
+      ],
+    },
+  });
 }
